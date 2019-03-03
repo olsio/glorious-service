b9ac338a99d480f11fe48153d20d34493d4f98f9
Oliver Schneider <os@ols.io>
2019-03-03 20:40:40 +0100

    Switching to parameters

--- src/main/scala/glo/routes/LoginRoute.scala
+++ src/main/scala/glo/routes/LoginRoute.scala
@@ -4,14 +4,15 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
-import glo.models.{ AuthResponse, JsonSupport }
+import glo.models.{AuthResponse, JsonSupport}
 
 trait LoginRoute extends JsonSupport {
 
   lazy val loginRoute: Route =
     path("login") {
       get {
-        entity(as[AuthResponse]) { response =>
+        parameters('code.as[String], 'state.as[String]) { (code, state) =>
+          val response = AuthResponse(code, state)
           complete(response)
         }
       }
